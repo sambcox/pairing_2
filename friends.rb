74744ae2ede10e635ec5d18f90c02dedbac0d225
Samuel Cox <samc1253@gmail.com>
2022-09-14 12:13:20 -0600

    Add another array

--- friends.rb
+++ friends.rb
@@ -2,3 +2,8 @@ arrays = ["Kyle", "Anthony", "Ryan", "Sam", "Kaitlyn"]
 puts arrays[2]
 puts arrays[4]
 puts arrays[0]
+
+places_to_visit = ["Como", "LA", "Austin", "Orlando", "Des Moines"]
+puts places_to_visit
+places_to_visit.push("New York")
+puts places_to_visit
